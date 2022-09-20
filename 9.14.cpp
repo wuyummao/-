8c368f4281e68abe93fe7169146964c9bb0c26c9
wuyummao <113826261+wuyummao@users.noreply.github.com>
2022-09-20 22:49:58 +0800

    Add files via upload

---
+++ 9.14.cpp
@@ -0,0 +1,146 @@
+﻿// 9.14.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+
+# include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	char arr1[] = "oknhygfd";
+//	char arr2[] = "########";
+//	int left = 0;
+//	int num = sizeof(arr1) / sizeof(arr1[0]);
+//	
+//	int right = num-2;//字符串减2，数组减1
+//	
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		left++;
+//		right--;
+//		printf("%s\n", arr2);
+//
+//	}
+//
+//
+//}
+
+
+
+//
+//int main()
+//{
+//
+//	int i = 1;
+//	int num = 1;
+//	int b = 0;
+//	scanf_s("%d",&b);
+//
+//	for (i = 1;i<b+1;i++)
+//	{
+//		num = num * i;
+//
+//	};
+//
+//printf("%d\n", num);
+//
+//
+//
+//
+//}
+//
+//
+//
+//
+
+
+//int main()
+//{
+//
+//	
+//	int num = 0;
+//
+//	scanf_s("%d", &num);
+//
+//	if (num%2 == 1)
+//		 printf("奇数\n");
+//	else
+//		 printf("偶数\n");
+//	
+//	return 0;
+//
+//};
+
+
+//int Max(int x, int y)
+// {
+//
+//if (x > y)
+//	return x;
+//else
+//	return y;
+//
+//}
+//
+//
+//int main()
+//{
+//
+//	int num1 = 45;
+//	int num2 = 634;
+//
+//	int max = 0;
+//	max = Max (num1, num2);
+//
+//	printf("%d", max);
+//
+//	return 0;
+//
+//
+//}
+
+
+//int main()
+//{
+//    int cnt = 0;
+//    for (int i = 1; i <= 2019; i++)
+//    {
+//        int m = i;
+//        while (m > 0)
+//        {
+//            if (m % 10 == 9)
+//            {
+//                cnt++;
+//                break;
+//            }
+//            else
+//            {
+//                m /= 10;
+//            }
+//        }
+//    }
+//    //输出
+//    printf("%d\n", cnt);
+//
+//    return 0;
+//}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
